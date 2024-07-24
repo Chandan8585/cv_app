b8e718811fccac03db4227a75f8a2e40a2f0c0d3
Chandan8585 <chandanwingshr@gmail.com>
2024-07-24 21:14:13 +0530

    debug error

--- cv-app/src/components/Body/Body.jsx
+++ cv-app/src/components/Body/Body.jsx
@@ -1,10 +1,8 @@
 import React, { useRef, useState } from "react";
 import ReactToPrint from "react-to-print";
 import { ArrowDown } from "react-feather";
-
-import Editor from "../Editor/Editor";
 import Resume from "../Resume/Resume";
-
+import Editor from "../Editor/Editor.jsx";
 import styles from "./Body.module.css";
 
 function Body() {
